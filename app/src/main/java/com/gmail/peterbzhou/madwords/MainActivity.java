9de21cf427ba34f2faef53887a05e6efa4e69ef4
Peter Zhou <peterbzhou@gmail.com>
2015-08-23 14:30:09 -0400

    Created Channel and implemented intent filter

--- app/src/main/java/com/gmail/peterbzhou/madwords/MainActivity.java
+++ app/src/main/java/com/gmail/peterbzhou/madwords/MainActivity.java
@@ -1,18 +1,41 @@
 package com.gmail.peterbzhou.madwords;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+
+    public Channel mChannel;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        WifiP2pManager mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
     }
 
     @Override
@@ -42,4 +65,6 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction("com.gmail.peterbzhou.SEARCHING");
         sendBroadcast(intent);
     }
+
+
 }
